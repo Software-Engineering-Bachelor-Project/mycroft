2a0dea8b08b41b210bb0730e9278d88d134026cb
SteBry <43108598+SteBry@users.noreply.github.com>
2020-03-30 14:46:55 +0200

    feat(urls): Created basic mapping between urls and views (#6)
    The urls and corresponding views need to be updated when implementing the modules,
    Integration tests need to be updated when views are implemented

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,28 @@
 from django.urls import path
 from . import views
+'''
+This file specifies the mapping between urls and views
 
-urlpatterns = []
+Note: The "name" parameter is used in tests to decouple the urls from tests. This mean that you can change the urls and 
+not affect the tests if the name parameter is unchanged
+'''
+
+app_name = 'backend'
+urlpatterns = [
+    path('filter', views.filter, name='filter'),
+
+    path('project/getall', views.project_get_all, name='project get all'),
+    path('project/save', views.project_save, name='project save'),
+    path('project/open', views.project_open, name='project open'),
+
+    path('export/filter', views.export_filter, name='export filter'),
+    path('export/videos', views.export_videos, name='export videos'),
+
+    path('video/get_info', views.video_get_info, name='video info'),
+    path('video/get_stream', views.video_get_stream, name='video stream'),
+
+    path('file/get_folders', views.file_get_folders, name='file get folders'),
+    path('file/add_folder', views.file_add_folders, name='file add'),
+
+    path('object_detection/detect_objects', views.detect_objects, name='detect objects'),
+]

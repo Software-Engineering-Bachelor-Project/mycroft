8839d8aa7a065c83bcda3297691176811594c76e
SteBry <43108598+SteBry@users.noreply.github.com>
2020-03-30 15:17:28 +0200

    test(frontend): Added dummy tests for frontend (#47)
    This prevents travis from failing tests

---
+++ frontend/src/tests/test_unit/dummy.test.js
@@ -0,0 +1,32 @@
+/*
+ * In this map unit tests should be written
+ * TODO: Remove this file once any other file with unit tests has been written
+ */
+
+
+/*
+ * In this file there are some example tests,
+ * A test file need to be named *.test.js inorder to be run by JEST (The testing framework)
+ *
+ * The naming convention is: (name of file/ component that is being tested).test.js
+ * The test files should be located in either the map test_integraton (integration test) or test_unit (unittest)
+ * depending on what type of test it is
+ */
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
+
+test('object assignment', () => {
+  const data = {one: 1};
+  data['two'] = 2;
+  expect(data).toEqual({one: 1, two: 2});
+});
+
+test('adding positive numbers is not zero', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});

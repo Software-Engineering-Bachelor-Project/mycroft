24c2f42bc4578691688265a93c20ed035e46b5c5
Alfred Sporre <alfred@familjensporre.se>
2020-04-01 14:34:29 +0200

    fix: refactored filenames on frontend
    We are now using camelCasing, with the first letter in lower case.
    Indents were also changed to 4 spaces.

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -9,12 +9,12 @@
 // .then(data => {}) where 'data' is the
 // received JSON-formatted data.
 export function makePOST(url, opts) {
-		return fetch(url, {
-				method: 'post',
-				headers: {
-						'Accept': 'application/json',
-						'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(opts)
-		}).then(r => {return r.json()});
+    return fetch(url, {
+	method: 'post',
+	headers: {
+	    'Accept': 'application/json',
+	    'Content-Type': 'application/json'
+	},
+	body: JSON.stringify(opts)
+    }).then(r => {return r.json()});
 }

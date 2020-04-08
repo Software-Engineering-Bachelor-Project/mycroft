9ab13a43192d7c4c3318aef4168a6697ac8d42b3
felfl076 <felfl076@student.liu.se>
2020-04-08 17:21:17 +0200

    feat: menu items

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 /* -- Menu -- */
 class Menu extends Component {
+    switchProject() {
+        console.log('Switch Project was clicked.');
+    }
+
+    manageSources() {
+        console.log('Manage Sources was clicked.');
+    }
+
+    exportJSON() {
+        console.log('Export JSON was clicked.');
+    }
+
+    exportClips() {
+        console.log('Export Clips was clicked.');
+    }
+
+    detectObjects() {
+        console.log('Object Detection was clicked.');
+    }
+
     render() {
         return (
-            <p> Menu Component </p>
+            <DropdownButton alignRight
+                            title="Project"
+                            id="dropdown-menu-align-right"
+            >
+              <Dropdown.Item onClick={() => this.switchProject()}> Switch Project </Dropdown.Item>
+              <Dropdown.Item onClick={() => this.manageSources()}> Manage Sources </Dropdown.Item>
+              <Dropdown.Item onClick={() => this.exportJSON()}> Export JSON </Dropdown.Item>
+              <Dropdown.Item onClick={() => this.exportClips()}> Export Clips </Dropdown.Item>
+              <Dropdown.Item onClick={() => this.detectObjects()}> Object detection </Dropdown.Item>
+            </DropdownButton>
         );
     }
 }

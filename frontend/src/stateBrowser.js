a130d43f27615b0b40cd11cc33aac1936e2bb25c
Alfred Sporre <alfsp631@student.liu.se>
2020-04-13 11:35:44 +0200

    feat: adds file browser
    Added class representation of backend folders.
    A temporary dictionary of folders has been added to the browser state.
    There are temporary functions used for generating these folder structures.
    The contents of this dictionary is expanded into a React Tree.

--- frontend/src/stateBrowser.js
+++ frontend/src/stateBrowser.js
@@ -1,13 +1,64 @@
 import store from './state';
 
-import { Camera, Clip } from './types';
-
+import { Camera, Clip, Folder } from './types';
 
 /* -- ACTIONS -- */
 const DUMMY = "DUMMY";
 
 /* -- INITIAL STATE -- */
+
+// Temporary functions for generating IDs.
+var ID_COUNTER_F = 1;
+function genIDF() {
+    return ID_COUNTER_F++;
+}
+
+var ID_COUNTER_C = 1;
+function genIDC() {
+    return ID_COUNTER_C++;
+}
+
+// Temporary function for generating folder structure.
+function genFolderTree(depth, fs=3, cs=3, parent=undefined) {
+    var id = genIDF();
+    var f = new Folder(id, 'Folder ' + id, parent);
+    
+    var numSubs = Math.ceil(Math.random() * fs);
+    var numClips = Math.ceil(Math.random() * cs);
+
+    if (depth > 0) {
+        for (var i = 0; i < numSubs; i++) {
+            var tf = genFolderTree(depth - 1, fs, cs, f);
+            f.folders = {
+                ...f.folders,
+                [tf.id]: tf
+            };
+        }
+    }
+
+    for (var j = 0; j < numClips; j++) {
+        var cid = genIDC();
+        var c = new Clip(cid, 'clip' + cid, f, 'wav', '69', '420');
+        f.clips = {
+            ...f.clips,
+            [c.id]: c
+        };
+    }
+
+    return f;
+}
+
+// Temporary root folders
+var FOLDER_1 = genFolderTree(2);
+var FOLDER_2 = genFolderTree(3);
+var FOLDER_3 = genFolderTree(4);
+
 const initialState = {
+    folders: {
+        [FOLDER_1.id]: FOLDER_1,
+        [FOLDER_2.id]: FOLDER_2,
+        [FOLDER_3.id]: FOLDER_3
+    },
     cameras: {
        1: new Camera(1, "cam1", [2,2], {
                1: new Clip(1, "clip1", "testfolder", "testformat", "start", "end"),
@@ -18,7 +69,7 @@ const initialState = {
        4: new Camera(4, "cam3", [6,9], {1: new Clip(1, "clip3", "testfolder", "testformat", "start", "end")}, false),
        5: new Camera(5, "cam3", [6,9], {1: new Clip(1, "clip3", "testfolder", "testformat", "start", "end")}, false),
    }
-}
+};
 
 /* -- ACTION CREATORS -- */
 export function dummy() {

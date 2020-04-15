d5eae043d2fe3f169593cdb3601dadc17affe8b8
Kal-Elx <kallej1johansson@gmail.com>
2020-04-15 16:12:30 +0200

    feat: management command for setting entry folder
    Created a management command for setting the entry folder.
    The command can be invoked with:
    python manage.py setentryfolder <absolute path>

---
+++ backend/management/commands/setentryfolder.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from backend.file_manager import build_file_structure
+
+
+class Command(BaseCommand):
+    help = 'Sets entry folder to given file path.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('folder', type=str, help='Path to entry folder')
+
+    def handle(self, *args, **kwargs):
+        folder = kwargs['folder']
+        build_file_structure(file_path=folder)
+        self.stdout.write("Added {} as entry folder.".format(folder))

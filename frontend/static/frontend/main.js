069c503072e841d304a311df8cdc3668c277f705
Alfred Sporre <alfsp631@student.liu.se>
2020-04-17 16:44:57 +0200

    refactor: makePOST rework

--- frontend/static/frontend/main.js
+++ frontend/static/frontend/main.js
@@ -66095,12 +66095,20 @@ __webpack_require__.r(__webpack_exports__);
  * This file contains helper-functions to be
  * used throughout Mycroft.
  */
-// This function is used to make post-requests.
-// Follow up a call to this function by using
-// .then(data => {}) where 'data' is the
-// received JSON-formatted data.
-function makePOST(url, opts) {
-  return fetch(url, {
+
+/**
+ * 
+ * This function is used to make POST-requests.
+ * 
+ * @param {string} url The sub-URL to send the request to. Will be appended onto the host URL.
+ * @param {JSON} opts The JSON data to send through the request.
+ * @param {fucntion} onResponse The function to be called upon request response. Expects two parameters: (status: int, data: JSON). If the request fails, 'data' will be undefined.
+ * 
+ */
+function makePOST(url) {
+  var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  var onResponse = arguments.length > 2 ? arguments[2] : undefined;
+  fetch(url, {
     method: 'post',
     headers: {
       'Accept': 'application/json',
@@ -66108,7 +66116,17 @@ function makePOST(url, opts) {
     },
     body: JSON.stringify(opts)
   }).then(function (r) {
-    return r.json();
+    // Status check
+    if (r.ok) {
+      console.log('POST SUCCESS: \'' + url + '\' | ' + r.status + ' - ' + r.statusText); // Call success function
+
+      r.json().then(function (data) {
+        return onResponse(r.status, data);
+      });
+    } else {
+      console.error('POST FAILURE: \'' + url + '\' | ' + r.status + ' - ' + r.statusText);
+      onResponse(r.status, undefined);
+    }
   });
 }
 

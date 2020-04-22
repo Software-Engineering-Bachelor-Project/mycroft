3d03f405530f60f6ca1d6cb01bc2cff1fdd2d1fc
Mattias Beming <mattias.beming@outlook.com>
2020-04-22 11:04:42 +0200

    feat: create glassbox on timeline
    Fixed so that getLinePlacements takes the startTime into account.
    Created function for glassbox that sets its timelimits (gbSetTimeLimits()).
    Created functions (2) in stateTimeline.js that checks time values, also updated old ones.
    Created tests for glassbox in timeline.test.js.
    Added simple css to glassbox.

---
+++ frontend/src/components/timeline/glassbox.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+import styles from './timeline.module.css';
+
+/**
+ * This class respresents the glassbox component.
+ */
+class Glassbox extends Component {
+
+    intervalChanged(start, end) {
+        //TODO: dispatch modifyFilter action using start and end time
+        this.props.gbSetTimeLimits(start, end);
+    }
+
+    render() {
+        return (
+            <div 
+                className={styles.glassbox}
+                style={{
+                    width: (this.props.gbTimeSpan / this.props.timeSpan) * 100 + "%",
+                    left: ((this.props.gbStartTime.getTime() - this.props.startTime.getTime()) / this.props.timeSpan) * 100 + "%"
+                }}
+            >
+
+            </div>
+        );
+    }
+}
+
+//Map Redux states to React props
+const mapStateToProps = state => {
+    return {
+        startTime: state.timeline.startTime,
+        endTime: state.timeline.endTime,
+        timeSpan: state.timeline.timeSpan,
+
+        //Glassbox
+        gbStartTime: state.timeline.glassbox.startTime,
+        gbEndTime: state.timeline.glassbox.endTime,
+        gbTimeSpan: state.timeline.glassbox.timeSpan
+    };
+};
+
+//Forward Redux's dispatch function to React props
+const mapDispatchToProps = dispatch => {
+    return {
+        gbSetTimeLimits: (startDate, endDate) => dispatch(gbSetTimeLimits(startDate, endDate))
+    };
+};
+
+//Connect Redux with React
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Glassbox);

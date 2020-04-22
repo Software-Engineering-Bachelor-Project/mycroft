c510575b381d836820a6b64a918f5efc4f0ece7c
Mattias Beming <mattias.beming@outlook.com>
2020-04-22 11:04:42 +0200

    feat: create glassbox on timeline
    Fixed so that getLinePlacements takes the startTime into account.
    Created function for glassbox that sets its timelimits (gbSetTimeLimits()).
    Created functions (2) in stateTimeline.js that checks time values, also updated old ones.
    Created tests for glassbox in timeline.test.js.
    Added simple css to glassbox.

--- frontend/src/components/timeline/timeline.js
+++ frontend/src/components/timeline/timeline.js
@@ -6,26 +6,34 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 import { zoom } from "../../state/stateTimeline";
 
 import styles from "./timeline.module.css";
+import Glassbox from './glassbox';
+
 
 /**
  * This function returns a list of line placements in percents.
  * Example: ["20%", "40%", "60%", "80%"]
- *
- * @param {int} timeSpan This is the state parameter timeSpan. (Located in stateTimeline.js)
+ * 
+ * @param {int} startTime The start date of timeline
+ * @param {int} timeSpan The time span from startTime to endTime.
  * @return {Array} List of line placements.
  */
-export function getLinePlacements(timeSpan) {
-  var hrs = timeSpan / (60 * 60 * 1000);
-  var step = 100 / hrs;
-  if (hrs <= 1) {
-    return [];
-  }
-  var list_ = new Array();
-  for (var j = 1; j < hrs; j++) {
-    list_.push(step * j + "%");
-  }
-  return list_;
-}
+export function getLinePlacements(startTime, timeSpan) {
+    var totalHrs = timeSpan / (60 * 60 * 1000);
+    var step = 100 / totalHrs;
+    var mintuesOffset = startTime.getMinutes();
+    var secondOffset = startTime.getSeconds();
+    var hrsOffset = (mintuesOffset*60 + secondOffset)/(60*60);
+    var percentOffset = (hrsOffset/totalHrs)*100;
+
+    if (totalHrs <= 1) {
+        return [];
+    }
+    var list_ = new Array();
+    for (var j = 1; j < totalHrs; j++) {
+        list_.push(((step * j) - percentOffset) + "%");
+    }
+    return list_;
+};
 
 // Scaling options for the dropdown menu
 const SCALE_LIST = [12, 24, 36, 48];
@@ -34,81 +42,88 @@ const SCALE_LIST = [12, 24, 36, 48];
  * This class respresents the timeline react-component.
  */
 class Timeline extends Component {
-  render() {
-    return (
-      <div className={styles.main}>
-        {/* This is the div for the topbar which contains the dropdown menu(s) */}
-        <div className={styles.topbar}>
-          {/* Dropdown menu for scaling the timeline */}
-          <DropdownButton
-            className={styles.dropdown}
-            title={this.props.scale + " Hours"}
-          >
-            {/* Create dropdown items for every scaling option */}
-            {SCALE_LIST.map((hrs) => {
-              return (
-                <Dropdown.Item onClick={(a) => this.props.zoom(hrs)} key={hrs}>
-                  {hrs + " Hours"}
-                </Dropdown.Item>
-              );
-            })}
-          </DropdownButton>
-        </div>
 
-        {/* This is the box containing all the timestamps, which is affected by scaling */}
-        <div className={styles.sliderbox}>
-          <div
-            className={styles.slider}
-            style={{
-              width:
-                (this.props.timeSpan / (60 * 60 * 1000) / this.props.scale) *
-                  100 +
-                "%",
-            }}
-          >
-            <div className={styles.day}>
-              <div className={styles.date}>Apr 17</div>
-            </div>
+    render() {
+        return (
+            <div className={styles.main}> 
 
-            {/* Creates a line for each timestamp and draws out hours*/}
-            {getLinePlacements(this.props.timeSpan).map((p, i) => {
-              return (
-                <div
-                  style={{
-                    position: "absolute",
-                    left: p,
-                    top: "0",
-                    height: "100%",
-                  }}
-                  key={p}
-                >
-                  <div className={styles.line}> </div>
-                  <div
-                    style={{
-                      position: "absolute",
-                      left: "5px",
-                      bottom: "2px",
-                    }}
-                  >
-                    {(this.props.startTime.getHours() + i + 1) % 24}
-                  </div>
+                {/* This is the div for the topbar which contains the dropdown menu(s) */}
+                <div className={styles.topbar}>
+
+                    {/* Dropdown menu for scaling the timeline */}
+                    <DropdownButton
+                        className={styles.dropdown}
+                        title={this.props.scale + " Hours"}
+                    >
+
+                        {/* Create dropdown items for every scaling option */}
+                        {SCALE_LIST.map((hrs) => {
+                            return (
+                                <Dropdown.Item onClick={(a) => this.props.zoom(hrs)} key={hrs}>
+                                    {hrs + " Hours"}
+                                </Dropdown.Item>
+                            );
+                        })}
+                    </DropdownButton>
                 </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    );
-  }
+                
+                {/* This is the box containing all the timestamps, which is affected by scaling */}
+                <div className={styles.sliderbox}>
+                    <div 
+                        className={styles.slider} 
+                        style={{width: (((this.props.endTime.getTime() - this.props.startTime.getTime()) / (60 * 60 * 1000)) / this.props.scale) * 100 + "%"}}
+                    >
+                        <div className={styles.day}>
+                            <div className={styles.date}>
+                                Apr 17
+                            </div>
+                        </div>
+
+                        {/* Glassbox component */}
+                        <Glassbox />
+
+                        {/* Creates a line for each timestamp and draws out hours*/}
+                        {getLinePlacements(this.props.startTime, this.props.timeSpan).map((p, i) => {
+                            return ( 
+                                <div
+                                    style={{
+                                        position: "absolute",
+                                        left: p,
+                                        top: "0",
+                                        height: "100%"
+                                    }} 
+                                    key={p}
+                                >    
+                                    
+                                    <div className={styles.line} > </div>                                    
+                                    <div 
+                                        style={{
+                                            position: "absolute",
+                                            left: "5px",
+                                            bottom: "2px"
+                                        }}
+                                    >
+                                        {(this.props.startTime.getHours() + i + 1) % 24}
+                                    </div>
+                                </div> 
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 //Map Redux states to React props
-const mapStateToProps = (state) => {
-  return {
-    scale: state.timeline.scale,
-    timeSpan: state.timeline.timeSpan,
-    startTime: state.timeline.startTime,
-  };
+const mapStateToProps = state => {
+    return {
+        startTime: state.timeline.startTime,
+        endTime: state.timeline.endTime,
+        scale: state.timeline.scale,
+        timeSpan: state.timeline.timeSpan,
+        startTime: state.timeline.startTime
+    };
 };
 
 //Forward Redux's dispatch function to React props

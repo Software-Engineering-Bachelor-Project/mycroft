d3bd3a6ea837d8e62c5869915cb0534c2402c032
Mattias Beming <mattias.beming@outlook.com>
2020-04-22 15:49:08 +0200

    fix: refactor code, feedback fixes
    Refactor code to 2 tab-spaces.
    
    glassbox.js:
    Added missing import for gbSetTimeLimits().
    Added constructor to bind intervalChanged().
    
    stateTimeline.js:
    Added comments for all export functions.
    Fixed a state copy error.
    
    timeline.test.js:
    Fixed a state copy error.

--- frontend/src/components/timeline/glassbox.js
+++ frontend/src/components/timeline/glassbox.js
@@ -1,56 +1,65 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import styles from './timeline.module.css';
+//import functions
+import { gbSetTimeLimits } from "./../../state/stateTimeline";
+
+//import css
+import styles from "./timeline.module.css";
 
 /**
  * This class respresents the glassbox component.
  */
 class Glassbox extends Component {
+  constructor(props) {
+    super(props);
+    this.intervalChanged = this.intervalChanged.bind(this);
+  }
 
-    intervalChanged(start, end) {
-        //TODO: dispatch modifyFilter action using start and end time
-        this.props.gbSetTimeLimits(start, end);
-    }
+  intervalChanged(start, end) {
+    //TODO: dispatch modifyFilter action using start and end time
+    this.props.gbSetTimeLimits(start, end);
+  }
 
-    render() {
-        return (
-            <div 
-                className={styles.glassbox}
-                style={{
-                    width: (this.props.gbTimeSpan / this.props.timeSpan) * 100 + "%",
-                    left: ((this.props.gbStartTime.getTime() - this.props.startTime.getTime()) / this.props.timeSpan) * 100 + "%"
-                }}
-            >
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className={styles.glassbox}
+        style={{
+          width: (this.props.gbTimeSpan / this.props.timeSpan) * 100 + "%",
+          left:
+            ((this.props.gbStartTime.getTime() -
+              this.props.startTime.getTime()) /
+              this.props.timeSpan) *
+              100 +
+            "%",
+        }}
+      ></div>
+    );
+  }
 }
 
 //Map Redux states to React props
-const mapStateToProps = state => {
-    return {
-        startTime: state.timeline.startTime,
-        endTime: state.timeline.endTime,
-        timeSpan: state.timeline.timeSpan,
+const mapStateToProps = (state) => {
+  return {
+    startTime: state.timeline.startTime,
+    endTime: state.timeline.endTime,
+    timeSpan: state.timeline.timeSpan,
 
-        //Glassbox
-        gbStartTime: state.timeline.glassbox.startTime,
-        gbEndTime: state.timeline.glassbox.endTime,
-        gbTimeSpan: state.timeline.glassbox.timeSpan
-    };
+    //Glassbox
+    gbStartTime: state.timeline.glassbox.startTime,
+    gbEndTime: state.timeline.glassbox.endTime,
+    gbTimeSpan: state.timeline.glassbox.timeSpan,
+  };
 };
 
 //Forward Redux's dispatch function to React props
-const mapDispatchToProps = dispatch => {
-    return {
-        gbSetTimeLimits: (startDate, endDate) => dispatch(gbSetTimeLimits(startDate, endDate))
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    gbSetTimeLimits: (startDate, endDate) =>
+      dispatch(gbSetTimeLimits(startDate, endDate)),
+  };
 };
 
 //Connect Redux with React
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Glassbox);
+export default connect(mapStateToProps, mapDispatchToProps)(Glassbox);

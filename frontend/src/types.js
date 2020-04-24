415b532c025a41cc2e87677eb309bbe4426411c8
Kal-Elx <kallej1johansson@gmail.com>
2020-04-24 14:22:07 +0200

    refactor: data structures

--- frontend/src/types.js
+++ frontend/src/types.js
@@ -38,10 +38,10 @@ export class Camera {
    * @param {int} id The unique identifier of this camera. Corresponds to the backend database.
    * @param {string} name The name of this camera.
    * @param {Object} pos The decimal longitude and latitude of this camera. The object has two attributes, latitude and longitude.
-   * @param {dict[int: Clip]} clips A dictionary containing this camera's clips, mapped by their own IDs.
+   * @param {Array[int]} clips A list containing this camera's clip's ID:s.
    * @param {boolean} selected Whether this camera is selected or not.
    */
-  constructor(id, name, pos, clips = {}, selected = false) {
+  constructor(id, name, pos, clips = [], selected = false) {
     this.id = id;
     this.name = name;
     this.pos = pos;
@@ -56,7 +56,7 @@ export class Camera {
    * @return {boolean} True if this camera does not contain any clips.
    */
   isEmpty() {
-    return Object.keys(this.clips).length == 0;
+    return this.clips.length == 0;
   }
 }
 
@@ -67,7 +67,7 @@ export class Clip {
   /**
    * @param {int} id The unique identifier of this clip. Corresponds to the backend database.
    * @param {string} name The file name of this clip, without file extension.
-   * @param {Folder} folder The path of the folder containing this clip.
+   * @param {Number} folder The id of the folder containing this clip.
    * @param {string} format The file format of this clip, 'wav' etc.
    * @param {Date} startTime The point in time when the clip starts.
    * @param {Date} endTime The point in time when the clip ends.
@@ -84,11 +84,13 @@ export class Clip {
   /**
    *
    * Calculates the complete file path, including containing folder and file extension.
-   *
+   * @param {Object[int, Folder]} Current folders in store.
    * @return {string} The full file path of this clip.
    */
-  getPath() {
-    return this.folder.getPath() + this.name + "." + this.format;
+  getPath(folders) {
+    return (
+      folders[this.folder].getPath(folders) + this.name + "." + this.format
+    );
   }
 }
 
@@ -100,25 +102,26 @@ export class Folder {
    * @param {int} id The unique identifier of this folder. Corresponds to the backend database.
    * @param {string} name The name of this folder.
    * @param {int} parent The id of the folder containing this folder. Should be undefined if root.
-   * @param {dict[int: Folder]} folders A dictionary containing subfolders, mapped by their own IDs.
-   * @param {dict[int: Clip]} clips A dictionary containing this folder's clips, mapped by their own IDs.
+   * @param {Array[int]} children ID:s to children folders.
+   * @param {Array[int]} clips ID:s to clips in the folder.
    */
-  constructor(id, name, parent = undefined, folders = {}, clips = {}) {
+  constructor(id, name, parent = undefined, children = [], clips = []) {
     this.id = id;
     this.name = name;
     this.parent = parent;
-    this.folders = folders;
+    this.children = children;
     this.clips = clips;
   }
 
   /**
    *
    * Calculates the full path of this folder recursively.
-   *
+   * @param {Object[int, Folder]} Current folders in store.
    * @return {string} the full path of this folder.
    */
-  getPath() {
-    if (this.parent == undefined) return this.name + "/";
-    else return this.parent.getPath() + this.name + "/";
+  getPath(folders) {
+    if (this.parent != undefined && this.parent in folders)
+      return folders[this.parent].getPath(folders) + this.name + "/";
+    else return this.name + "/";
   }
 }

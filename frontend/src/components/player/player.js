f711c1cbf4f2767bba1039524a834d88901cee41
SteBry <stefan.brynielsson@gmail.com>
2020-04-27 10:47:23 +0200

    feat: implement play, pause and jump actions for player

--- frontend/src/components/player/player.js
+++ frontend/src/components/player/player.js
@@ -1,34 +1,72 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import styles from "../viewport/viewport.module.css";
+import { setPosition, setPlayer, play, pause } from "../../state/statePlayer";
+
+import "../../../node_modules/video-react/dist/video-react.css"; // import css
+import {
+  Player as ReactPlayer,
+  ControlBar,
+  ReplayControl,
+  ForwardControl,
+  CurrentTimeDisplay,
+  TimeDivider,
+  PlaybackRateMenuButton,
+  VolumeMenuButton,
+} from "video-react";
 
 /* -- Player -- */
 class Player extends Component {
-  renderVideo(props) {
-    /**
-     * Render video if a clip id has been set
-     */
+  constructor(props) {
+    super(props);
+  }
 
-    const clipID = props.clipID;
-
-    if (clipID) {
-      return (
-        <video key={clipID} width="100%" height="100%" controls autoPlay>
-          <source
-            key={clipID}
-            src={"http://127.0.0.1:8000/video/stream/" + clipID + "/"}
-            type="video/mp4"
-          />
-        </video>
-      );
+  // Load the source of the player if it updates
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.clipID !== prevProps.clipID) {
+      this.player.load();
     }
-    return <h2 style={{ color: "white" }}>No video selected</h2>;
+  }
+
+  componentDidMount() {
+    this.player.subscribeToStateChange(this.handleStateChange.bind(this));
+    this.props.setPlayer(this.player);
+  }
+
+  handleStateChange(state) {
+    // Update states in store based on state of player
+    // Update the current position state
+    this.props.setPosition(state.currentTime);
+
+    // Update the playing state
+    if (state.paused && this.props.playing) {
+      play();
+    } else if (!state.paused && !this.props.playing) {
+      pause();
+    }
   }
 
   render() {
     return (
       <React.Fragment>
-        <this.renderVideo clipID={this.props.clipID} />
+        <ReactPlayer
+          ref={(player) => {
+            this.player = player;
+          }}
+        >
+          <source
+            src={
+              "http://127.0.0.1:8000/video/stream/" + this.props.clipID + "/"
+            }
+          />
+          <ControlBar>
+            <ReplayControl seconds={10} order={1.1} />
+            <ForwardControl seconds={30} order={1.2} />
+            <CurrentTimeDisplay order={4.1} />
+            <TimeDivider order={4.2} />
+            <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+            <VolumeMenuButton disabled />
+          </ControlBar>
+        </ReactPlayer>
       </React.Fragment>
     );
   }
@@ -38,12 +76,20 @@ class Player extends Component {
 const mapStateToProps = (state) => {
   return {
     clipID: state.player.clipID,
+    time: state.player.clipTime,
+    playing: state.player.playing,
+    player: state.player.player,
   };
 };
 
 // Forward Redux's dispatch function to React props
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    setPosition: (time) => dispatch(setPosition(time)),
+    setPlayer: (player) => dispatch(setPlayer(player)),
+    play: () => dispatch(play()),
+    pause: () => dispatch(pause()),
+  };
 };
 
 // Connect Redux with React
--- frontend/src/state/statePlayer.js
+++ frontend/src/state/statePlayer.js
@@ -3,24 +3,100 @@ import { ADD_CAMERA } from "./stateMap";
 
 /* -- ACTIONS -- */
 export const PLAY_CLIP = "PLAY_CLIP";
+export const PLAY = "PLAY";
+export const PAUSE = "PAUSE";
+export const SET_POSITION = "SET_POSITION";
+export const SET_PLAYER = "SET_PLAYER";
+export const JUMP = "JUMP";
 
 /* -- INITIAL STATE -- */
 export const initialState = {
   clipID: null,
+  position: 0,
+  playing: false,
+  player: null,
 };
 
 /* -- ACTION CREATORS -- */
 /**
  *
- * Creates an action used for playing a clip
+ * Creates an action used for setting which clip should be played
  *
  * @param {int} clipID id of clip to play.
- * @return {action} Action for adding a camera.
+ * @return {action} Action for setting a clip tobe played in the player.
  */
 export function playClip(clipID) {
   return {
     type: PLAY_CLIP,
     clipID: clipID,
+  };
+}
+
+/**
+ *
+ * Creates an action used for pausing the clip playing in the player
+ * NOTE: This should not be used for jumping in a clip, This should only be changed in the player component and is
+ * read-only at other places
+ *
+ * @param {float} position, the position in the clip currently playing
+ * @return {action} Action for setting the position
+ */
+export function setPosition(position) {
+  return {
+    type: SET_POSITION,
+    position: position,
+  };
+}
+/**
+ *
+ * Creates an action used for pausing the clip currently playing
+ *
+ * @return {action} Action for pausing a clip.
+ */
+export function pause() {
+  return {
+    type: PAUSE,
+  };
+}
+
+/**
+ *
+ * Creates an action used for pausing the clip currently playing
+ *
+ * @return {action} Action for pausing a clip.
+ */
+export function play() {
+  return {
+    type: PLAY,
+  };
+}
+
+/**
+ *
+ * Creates an action used for setting the player that plays the clip the clip
+ * NOTE!: Should only be used by the player component
+ *
+ * @param {Player} player, the position in the clip currently playing
+ * @return {action} Action for setting the player
+ */
+export function setPlayer(player) {
+  return {
+    type: SET_PLAYER,
+    player: player,
+  };
+}
+
+/**
+ *
+ * Jump a specified amount of seconds backward or forward in the clip
+ *
+ * @param {float} timeDelta the amount of seconds the position in the clip should be changed
+ * @return {action} Action for setting the player
+ */
+export function jump(timeDelta) {
+  return {
+    type: JUMP,
+    timeDelta: timeDelta,
   };
 }
 
@@ -32,6 +108,38 @@ const playerReducer = (state = initialState, action) => {
         ...state,
         clipID: action.clipID,
       };
+
+    case SET_POSITION:
+      return {
+        ...state,
+        position: action.position,
+      };
+    case PLAY:
+      if (state.player != null) {
+        state.player.play();
+      }
+      return {
+        ...state,
+        playing: true,
+      };
+    case PAUSE:
+      if (state.player != null) {
+        state.player.pause();
+      }
+      return {
+        ...state,
+        playing: false,
+      };
+    case SET_PLAYER:
+      return {
+        ...state,
+        player: action.player,
+      };
+    case JUMP:
+      const { player } = state.player.getState();
+      state.player.seek(player.currentTime + action.timeDelta);
+      return { ...state };
+
     default:
       return state;
   }

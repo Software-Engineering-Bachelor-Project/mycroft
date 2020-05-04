9772e389cc500292f350453c0212c8c5358270fd
felfl076 <felfl076@student.liu.se>
2020-05-04 12:19:34 +0200

    fix: cameras clip_set only contains clip in project

--- backend/test/test_unit/test_video_manager.py
+++ backend/test/test_unit/test_video_manager.py
@@ -52,19 +52,22 @@ class GetSequentialClipTest(TestCase):
 
 class GetCamerasTest(TestCase):
 
+    @patch('backend.video_manager.remove_clip_not_in_project_from_camera')
     @patch('backend.video_manager.os_aware')
     @patch('backend.video_manager.serialize')
     @patch('backend.video_manager.get_all_cameras_in_project')
-    def test_basic(self, mock_get_all_cameras_in_project, mock_serialize, mock_os_aware):
+    def test_basic(self, mock_get_all_cameras_in_project, mock_serialize, mock_os_aware,
+                   mock_remove_clip_not_in_project_from_camera):
         """
         Makes a simple call.
         """
         mock_get_all_cameras_in_project.return_value = "RETURNED CAMERAS"
         mock_serialize.return_value = 'RETURN FROM SERIALIZE'
+        mock_remove_clip_not_in_project_from_camera.return_value = "RETURNED MODIFIED CAMERAS"
         code, res = get_cameras(data={PROJECT_ID: 42})
         mock_get_all_cameras_in_project.assert_called_once_with(pid=42)
         mock_serialize.assert_called_once_with("RETURNED CAMERAS")
-        mock_os_aware.assert_called_once_with({CAMERAS: 'RETURN FROM SERIALIZE'})
+        mock_os_aware.assert_called_once_with('RETURNED MODIFIED CAMERAS')
         self.assertEqual(code, 200)
 
     def test_missing_parameter(self):
